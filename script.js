977f51a27ed7e31e62b9cb98d752639cb86c957d
DarshanKrishna-DK <darshankrishna2k2@gmail.com>
2023-10-21 08:03:46 +0530

    Dashboard Created

---
+++ script.js
@@ -0,0 +1,12 @@
+/*----------------Scroll Reveal----------------*/
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.blockchain h2', { origin: 'top'});
+ScrollReveal().reveal('.blockchain p', { origin: 'left'});
+ScrollReveal().reveal('.quiz-levles', { origin: 'right'});
